bfe5534e2db5fa4cf28b011ecd674eb3fe8c64ae
D1348965 <d1348965@o365.fcu.edu.tw>
2026-06-05 14:06:14 +0800

    fix: 更新快速連結與下載區域的連結地址

--- src/components/QuickLinks.vue
+++ src/components/QuickLinks.vue
@@ -1,43 +1,34 @@
-<script setup>
-// QuickLinks Component
-</script>
-
 <template>
   <div class="clearfix">
     <div class="books">
       <h2>必讀手冊</h2>
       <div class="list_box">
         <ul>
-          <li class="clearfix"><span>1</span><a href="#" target="_blank">電子帳簿自動遞送服務功能說明....</a></li>
-          <li class="clearfix"><span>2</span><a href="#" target="_blank">網銀小秘書</a></li>
-          <li class="clearfix"><span>3</span><a href="#" target="_blank">忘記密碼與代號了嗎</a></li>
-          <li class="clearfix"><span>4</span><a href="#" target="_blank">交易時間列表</a></li>
+          <li class="clearfix"><span>1</span><a href="https://netbank.hncb.com.twnotice/autoprocsys.html" target="_blank">電子帳簿自動遞送服務功能說明....</a></li>
+          <li class="clearfix"><span>2</span><a href="http://www.hncb.com.tw/ibankqa/index.shtml" target="_blank">網銀小秘書</a></li>
+          <li class="clearfix"><span>3</span><a href="http://www.hncb.com.tw/ibankqa/LoginLogout.shtml" target="_blank">忘記密碼與代號了嗎</a></li>
+          <li class="clearfix"><span>4</span><a href="https://netbank.hncb.com.tw/netbank/pages/jsp/Time/html/time_edit.jsp" target="_blank">交易時間列表</a></li>
         </ul>
       </div>
     </div>
-    
     <div class="download">
       <h2>軟體下載</h2>
       <div class="list_box">
         <ul>
-          <li class="clearfix"><span>1</span><a href="#" target="_blank">憑證小秘書</a></li>
+          <li class="clearfix"><span>1</span><a href="https://event.hncb.com.tw/ibankqa/cfca_nonset_tw.shtml" target="_blank">憑證小秘書</a></li>
         </ul>
       </div>
     </div> 
-    
     <div class="link">
       <h2>快速連結</h2>
       <div class="list_box">
         <ul class="clearfix">
-          <li><a href="#" class="link_03">線上申請專區</a></li>
+          <li><a href="https://pfpa.hncb.com.tw/pfpaip/web/" class="link_03">線上申請專區</a></li>
           <li><a href="#" class="link_06" target="_blank">e手包辦<br>繳費網</a></li>
-          <li><a href="#" class="link_05" target="_blank">華得來點數<br>兌換平台</a></li>
+          <li><a href="https://www.hnpoint.com.tw/" class="link_05" target="_blank">華得來點數<br>兌換平台</a></li>
           <li><a href="#" class="link_04">網路ATM</a></li>
         </ul>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-</style>
