2484a049e0aa882445517eb4b3c31521c01c9fba
D1348965 <d1348965@o365.fcu.edu.tw>
2026-06-05 16:42:14 +0800

    fix: update base url for github pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/vue-sfc/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/homework-5-vue/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
