ffb948b1c223a24928b39f541f1544f1da1c45b7
D1348965 <d1348965@o365.fcu.edu.tw>
2026-06-05 13:55:46 +0800

    Add CSS files for jQuery UI, bxSlider, and keyboard functionality
    - Created jquery-ui.min.css for styling jQuery UI components.
    - Added jquery.bxslider.css for bxSlider layout and controls.
    - Introduced keyboard.css for custom keyboard UI elements and interactions.

--- vue-sfc/src/main.js
+++ vue-sfc/src/main.js
@@ -1,4 +1,8 @@
-import './assets/main.css'
+import './assets/css/css.css'
+import './assets/css/font-awesome.css'
+import './assets/css/jquery-ui.min.css'
+import './assets/css/keyboard.css'
+import './assets/css/jquery.bxslider.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
